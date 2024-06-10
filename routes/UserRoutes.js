933132642f68aa0c6909f88db3fcd4f7611f02cb
Shivam <st4272333@gmail.com>
2024-06-10 09:47:16 +0530

    small change

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import checkUserAuth from "../middlewares/user-auth-middleware.js";
-import AuthController from "../controllers/user/authController.js";
+import AuthController from "../controllers/user/AuthController.js";
 import uploadProduct from "../middlewares/multer/ImageProduct.js";
 
 
@@ -20,10 +20,6 @@ routers.post("/verify",AuthController.verify)
 // protected routes 
 routers.get("/get-user", checkUserAuth , AuthController.getLoginUser);
 routers.post("/update-profile",uploadProduct('public/user/images').single("profile_pic"),checkUserAuth,AuthController.updateProfile);
-
-
-
-
 
 
 
